28d29f9d1aae7cd894d12537e30e77d7a07eb15c
kumarkartikmk57 <kumarkartikmk57@gmail.com>
2024-04-29 18:35:41 +0530

    added kubernetes files along with helm

---
+++ busybox_deployment/chart_creator.sh
@@ -0,0 +1,28 @@
+#export GIT_COMIT_ID="12azb" is the command used to export environment variable
+VERSION=$GIT_COMIT_ID
+cat << EOF > Chart.yaml
+apiVersion: v2
+name: busybox_deployment
+description: A Helm chart for Kubernetes
+
+# A chart can be either an 'application' or a 'library' chart.
+#
+# Application charts are a collection of templates that can be packaged into versioned archives
+# to be deployed.
+#
+# Library charts provide useful utilities or functions for the chart developer. They're included as
+# a dependency of application charts to inject those utilities and functions into the rendering
+# pipeline. Library charts do not define any templates and therefore cannot be deployed.
+type: application
+
+# This is the chart version. This version number should be incremented each time you make changes
+# to the chart and its templates, including the app version.
+# Versions are expected to follow Semantic Versioning (https://semver.org/)
+version: $VERSION
+
+# This is the version number of the application being deployed. This version number should be
+# incremented each time you make changes to the application. Versions are not expected to
+# follow Semantic Versioning. They should reflect the version the application is using.
+# It is recommended to use it with quotes.
+appVersion: "2.4-alpine"
+EOF
